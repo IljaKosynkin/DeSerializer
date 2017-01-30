842997cacb4ede2d7902fbd4f022127772c15908
iljakosynkin <ilja.kosynkin@gmail.com>
2017-01-30 17:51:02 +0100

    Initial Commit

---
+++ DeSerializer/DeSerializer.h
@@ -0,0 +1,19 @@
+//
+//  DeSerializer.h
+//  DeSerializer
+//
+//  Created by Ilja Kosynkin on 1/30/17.
+//  Copyright © 2017 Ilja Kosynkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeSerializer.
+FOUNDATION_EXPORT double DeSerializerVersionNumber;
+
+//! Project version string for DeSerializer.
+FOUNDATION_EXPORT const unsigned char DeSerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeSerializer/PublicHeader.h>
+
+
